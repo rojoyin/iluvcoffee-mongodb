f5b4caa226422c0dc5ed5ea2549fb3488e7ef473
rojoyin <rojoyin@gmail.com>
2023-01-30 16:33:42 -0500

    Format file

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -2,6 +2,6 @@ import { Module } from '@nestjs/common';
 import { CoffeesController } from './coffees.controller';
 
 @Module({
-  controllers: [CoffeesController]
+  controllers: [CoffeesController],
 })
 export class CoffeesModule {}
