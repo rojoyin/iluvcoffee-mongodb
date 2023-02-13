9035b07efb890df18d9af093a7d522c5e421f245
rojoyin <rojoyin@gmail.com>
2023-02-13 14:42:09 -0500

    Format source code

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,4 +1,4 @@
-import {Injectable, NotFoundException} from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Coffee } from './entities/coffee.entity';
 import { Model } from 'mongoose';
@@ -12,7 +12,6 @@ export class CoffeesService {
   findAll() {
     return this.coffeeModel.find().exec();
   }
-
 
   async findOne(id: string) {
     const coffee = await this.coffeeModel.findOne({ _id: id }).exec();
